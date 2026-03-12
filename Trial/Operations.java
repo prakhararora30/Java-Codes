4cd3c3450a2adc7236bbc2b60162d887238a7c73
Prakhar <prakhar.akgec@outlook.com>
2026-03-12 22:47:26 +0530

    Create Operations.java

---
+++ Trial/Operations.java
@@ -0,0 +1,26 @@
+class Operations{
+    static int add(int a, int b){
+        return a + b;
+    }
+
+    static int subtract(int a, int b){
+        return a - b;
+    }
+
+    static int multiply(int a, int b){
+        return a * b;
+    }
+
+    static int divide(int a, int b){
+        return a / b; 
+    }
+
+    public static void main(String[] args){
+        int a = 15;
+        int b = 10;
+        System.out.println(add( a, b));
+        System.out.println(subtract( a, b));
+        System.out.println(multiply( a, b));
+        System.out.println(divide( a, b));
+    }
+}
